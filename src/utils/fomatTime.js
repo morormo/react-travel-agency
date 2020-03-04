1092c5bdd2813ecfd5fd7878d7d09d7f8913e0ce
Daniel Stoklosa <morormo@gmail.com>
2020-03-04 11:49:18 +0100

    ADD Format Time

--- src/utils/fomatTime.js
+++ src/utils/fomatTime.js
@@ -1,1 +1,11 @@
-export const formatTime = () => 'formatted time';
+export const formatTime = time => {
+  if(time === undefined || isNaN(time) || time <0){
+    return null;
+  } else {
+    const second = Math.floor(time % 60).toString();
+    const minutes = Math.floor((time / 60)%60).toString();
+    const hour = Math.floor((time /3600) % 60).toString();
+
+    return hour.padStart(2,'0') + ':'+ minutes.padStart(2,'0')+ ':'+ second.padStart(2,'0');
+  }
+};
